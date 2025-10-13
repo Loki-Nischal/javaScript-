006b65af8ce4d76da872e44da6988a977707f577
Loki-Nischal <budhathoki.nischal@gmail.com>
2025-10-13 10:10:37 +0545

    error handling

---
+++ error_handling.js
@@ -0,0 +1,11 @@
+// try/catch
+// try -> include code that throws error
+//catch => catch error thrown by try block
+
+try {
+    console.log(a) // throws error
+} catch (error) {
+    console.log(error)
+}
+// try catch use garyo bhane error aayo bhane pani baki ko code lai aasar gardaina
+console.log("end of code")
